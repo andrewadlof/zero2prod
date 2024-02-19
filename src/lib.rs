084e69e93fab1e2d1289b5caa6ee1841885c9613
Drew Adlof <drew.adlof@gmail.com>
2024-02-18 23:14:30 -0600

    Add progress

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+ //Noticethe differentsignature!
+ //We return `Server` onthe happypath andwedropped the`async` keyword
+ //We have no .awaitcall, soit isnotneeded anymore.
+ pub fn run(listener: TcpListener)-> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .listen(listener)?
+    .run();
+    // No .await here!
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+use zero2prod::configuration::get_configuration;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main()-> Result<(), std::io::Error> {
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    // We have removed the hard-coded `0000` - it's now coming fom our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run(listener)?.await
 }
